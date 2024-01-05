050d82ca44c17b007405d5300ba64401b263c6e1
Leandro Borges <leandroborges_br@hotmail.com>
2024-01-05 19:23:16 +0000

    Added docker file

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,11 +4,11 @@ echo "=============================="
 
 git config --global user.name "${GITHUB_ACTOR}"
 git config --global user.email "${INPUT_EMAIL}"
-git config --global --add safe.directory /github/workspace
+git config --global --add safe.directory /github/workspaceg
 
 python3 /usr/bin/feed.py
 
-git add - A && git commit "Updated Feed"
+git add -A && git commit "Updated Feed"
 
 git push --set-upstream origin main
 
